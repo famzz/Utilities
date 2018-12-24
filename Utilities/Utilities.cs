6443274f56745fbe725700d1d542c216411afe1a
Abdul Al-Fahim <4104094+famzz@users.noreply.github.com>
2018-12-24 23:32:26 +0000

    Add project files.

---
+++ Utilities/Utilities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class MessageHandler
+    {
+
+        public static string GetMessage(NetworkStream stream)
+        {
+            byte[] lengthData = new byte[4];
+            int length = 0;
+
+            stream.Read(lengthData, 0, 4);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(lengthData);
+
+            length = BitConverter.ToInt32(lengthData, 0);
+
+            byte[] data = new byte[length];
+
+            stream.Read(data, 0, length);
+
+            return Encoding.ASCII.GetString(data);
+        }
+
+        public static void SendMessage(NetworkStream stream, string message)
+        {
+            int length = message.Length;
+
+            byte[] intBytes = BitConverter.GetBytes(length);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(intBytes);
+            byte[] lengthBytes = intBytes;
+
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+            byte[] data = new byte[lengthBytes.Length + messageBytes.Length];
+
+            Buffer.BlockCopy(lengthBytes, 0, data, 0, lengthBytes.Length);
+            Buffer.BlockCopy(messageBytes, 0, data, lengthBytes.Length, messageBytes.Length);
+
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
